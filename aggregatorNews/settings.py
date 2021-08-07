605c029a3691af061beab06ab7905b6a6e492e65
Maxim Kovalev <thelaimm@gmail.com>
2021-08-07 22:05:49 +0300

    change database

--- aggregatorNews/settings.py
+++ aggregatorNews/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'aggregatorNews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
