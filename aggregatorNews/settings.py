60ac967f3437f3bc50caf4465943980e80d7a93d
Maxim Kovalev <thelaimm@gmail.com>
2021-08-07 13:32:29 +0300

    add ui

--- aggregatorNews/settings.py
+++ aggregatorNews/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'aggregatorNews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'mainapp-ui/build']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = (
+    (BASE_DIR / 'mainapp-ui/build/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
